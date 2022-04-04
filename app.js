40021e96777e6ff90a8b11582b423512506d38fc
ajswebdesigns <andrew@ajswebdesigns.com>
2022-04-04 16:52:12 -0400

    getting there buddy

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require('mongoose')
+
 
 const app = express();
 
@@ -11,14 +10,42 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true});
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model('Item', itemsSchema);
+
+const item1 = new Item({
+  name: 'Welcome to your todolist!'
+});
+
+const item2 = new Item({
+  name: 'Hit the + button to add a new item!'
+});
+
+const item3 = new Item({
+  name: 'Hit this to delete an item'
+});
+
+
+// const defaultItems = [item1,item2,item3];
+// Item.insertMany(defaultItems).then(()=>{
+//   console.log('data inserted')
+// }).catch((err)=>{
+//   console.log('Oh no!',err)
+// })
+
+
 
 app.get("/", function(req, res) {
+let items = Item.find({});
+console.log(items)
 
-const day = date.getDate();
 
-  res.render("list", {listTitle: day, newListItems: items});
+  res.render("list", {listTitle: "Today", newListItems: items});
 
 });
 
